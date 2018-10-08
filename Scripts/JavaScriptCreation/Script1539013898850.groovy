5fcb0ccabe4105b983570c86bb94ff862a12e18a
girishk <girishk@ULTP_547.synerzipune.local>
2018-10-08 22:45:18 +0530

    Added action class and select class successfully...

--- Scripts/JavaScriptCreation/Script1539013898850.groovy
+++ Scripts/JavaScriptCreation/Script1539013898850.groovy
@@ -2,7 +2,11 @@ import java.util.concurrent.TimeUnit
 
 import org.openqa.selenium.By
 import org.openqa.selenium.WebDriver
+import org.openqa.selenium.WebElement
 import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.interactions.Actions
+import org.openqa.selenium.support.ui.Select
+
 import com.kms.katalon.core.webui.driver.DriverFactory
 
 System.setProperty("webdriver.chrome.driver", "E:\\ChromeDriver\\chromedriver.exe");
@@ -17,3 +21,19 @@ driver.get("https://opensource-demo.orangehrmlive.com/index.php/dashboard")
 driver.findElement(By.id("txtUsername")).sendKeys("Admin")
 driver.findElement(By.id("txtPassword")).sendKeys("admin123")
 driver.findElement(By.id("btnLogin")).click()
+
+WebElement element = driver.findElement(By.xpath("//b[contains(text(),'Leave')]"))
+Actions action = new Actions(driver)
+action.moveToElement(element).build().perform()
+
+Thread.sleep(5000)
+
+driver.findElement(By.xpath("//a[@id='menu_leave_viewLeaveList']")).click()
+
+
+Select dropdown  = new Select(driver.findElement(By.id("leaveList_cmbSubunit")))
+
+dropdown.selectByIndex(2);
+Thread.sleep(5000)
+
+driver.close()
